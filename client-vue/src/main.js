75ec643786d8babcf757399b35086c11cd3ef2a3
suryaajis330 <suryaajis330@gmail.com>
2022-01-20 13:06:50 +0700

    try to remember vue

--- client-vue/src/main.js
+++ client-vue/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import GSignInButton from 'vue-google-signin-button'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
+Vue.use(GSignInButton)
+Vue.use(VueMaterial)
 
 new Vue({
   router,
--- client-vue/src/views/About.vue
+++ client-vue/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <md-content>Background</md-content>
+    <md-content class="md-primary">Primary</md-content>
+    <md-content class="md-accent">Accent</md-content>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style>
+#about .md-content {
+  width: 200px;
+  height: 200px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
